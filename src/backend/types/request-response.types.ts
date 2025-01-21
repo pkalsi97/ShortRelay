ac3c150ca53f7dcaef49f228782b6f109b7131bf
pkalsi97 <pranjulkalsi@gmail.com>
2025-01-21 13:54:07 +0000

    feat: add generic request-response interfaces

---
+++ src/backend/types/request-response.types.ts
@@ -0,0 +1,13 @@
+export interface Response<T=unknown>{
+    success: boolean;
+    message?: string;
+    data?: T;
+}
+
+export interface Request {
+    headers?: {
+        authorization?: string;
+        'x-access-token'?: string;
+    };
+    body?:Record<string,unknown>;
+}
